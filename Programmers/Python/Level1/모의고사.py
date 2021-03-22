f2cf350fbc56508ad44f3ad5a9536d5d3cd3e100
carnival77 <stam0325@hanmail.net>
2021-03-22 22:30:29 +0900

    solving problem 2

--- Programmers/Python/Level1/모의고사.py
+++ Programmers/Python/Level1/모의고사.py
@@ -32,10 +32,6 @@ def solution(answers):
     return list(map(lambda x: x + 1, answer))
 
 
-
-    # return answer
-
-
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
     answer=[1,3,2,4,2] # 0,1,3,3,5,6. 3
