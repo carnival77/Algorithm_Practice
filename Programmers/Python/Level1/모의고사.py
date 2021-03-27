2669d401c92677b61b00987eb441a4ff19069c67
carnival77 <stam0325@hanmail.net>
2021-03-27 23:19:25 +0900

    solving 모의고사

--- Programmers/Python/Level1/모의고사.py
+++ Programmers/Python/Level1/모의고사.py
@@ -1,35 +1,26 @@
 def solution(answers):
-    
-    answer = []
 
     supo1 = [1, 2, 3, 4, 5]
     supo2 = [2, 1, 2, 3, 2, 4, 2, 5]
     supo3 = [3, 3, 1, 1, 2, 2, 4, 4, 5, 5]
 
-    correct = [0] * 3
+    correct = [0,0,0]
 
     for i, a in enumerate(answers):
-        if a == supo1[i]:
+        if a == supo1[i%len(supo1)]:
             correct[0] = correct[0] + 1
-        if a == supo2[i]:
-            correct[1] = correct[2] + 1
-        if a == supo3[i]:
+        if a == supo2[i%len(supo2)]:
+            correct[1] = correct[1] + 1
+        if a == supo3[i%len(supo3)]:
             correct[2] = correct[2] + 1
 
-    corrects = []
+    answer=[]
 
-    for i, n in enumerate(correct):
-        corrects.append((i, n))
+    for person,score in enumerate(correct):
+        if max(correct)== score:
+            answer.append(person+1)
 
-    # print(corrects)
-
-    corrects.sort(key=lambda x: x[1], reverse=True)
-
-    for x in corrects:
-        if corrects[0][1] <= x[1]:
-            answer.append(x[0])
-
-    return list(map(lambda x: x + 1, answer))
+    return answer
 
 ##
 # Press the green button in the gutter to run the script.
