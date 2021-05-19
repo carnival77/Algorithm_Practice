6be1111e01041f51ee37a08924143fc84d09b3bf
carnival77 <stam0325@hanmail.net>
2021-05-19 22:25:17 +0900

    solved Dynamic Programming 3

--- This_is_CodingTest/Dynamic Programming/개미 전사.py
+++ This_is_CodingTest/Dynamic Programming/개미 전사.py
@@ -10,3 +10,18 @@ for i in range(2,n):
     d[i] = max(d[i-1],d[i-2]+arr[i])
 
 print(d[n-1])
+
+
+
+# n=int(input())
+#
+# arr = list(map(int,input().split()))
+#
+# d=[0]*101
+#
+# d[0] = arr[0]
+# d[1]=arr[1]
+# for i in range(2,n+1):
+#     d[i] = max(d[i-1],d[i-2]+arr[i])
+#
+# print(d[n])
