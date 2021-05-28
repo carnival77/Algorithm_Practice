2016d3e420480eaa7aa00a01db402df525f6e25f
carnival77 <stam0325@hanmail.net>
2021-05-28 15:11:06 +0900

    studied Shortest Path Problems

--- This_is_CodingTest/Shortest Path/전보.py
+++ This_is_CodingTest/Shortest Path/전보.py
@@ -3,10 +3,8 @@ import sys
 input = sys.stdin.readline
 INF = int(1e9) # 무한을 의미하는 값으로 10억 설정
 
-# 노드, 간선 개수 입력
-n,m = map(int,input().split())
-# 시작 노드 번호 입력
-start = int(input())
+# 노드, 간선 개수, 시작번호 입력
+n,m,start = map(int,input().split())
 # 각 노드별 연결 정보 리스트 입력
 graph = [[] for i in range(n+1)]
 # 최단 거리 테이블. 초기값은 모두 무한
@@ -46,10 +44,12 @@ def dijkstra(start):
 # start 번호의 노드부터 다익스트라 알고리즘 수행
 dijkstra(start)
 
-# 모든 노드로 가기 위한 최단 거리를 출력
-for i in range(1,n+1):
-    # 도달할 수 없는 경우, 무한(INF)이라고 출력
-    if distance[i] == INF:
-        print("INF")
-    else:
-        print(distance[i])
+count=0
+max_dist = 0
+
+for d in distance:
+    if d != INF:
+        count+=1
+        max_dist = max(max_dist,d)
+
+print(count-1, max_dist)
