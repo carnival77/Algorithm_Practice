a1f13ecfa04e40a2475a694d1fcb5d13dcf88eb0
carnival77 <stam0325@daum.net>
2021-06-06 18:38:12 +0900

    solved greedy practices 4

---
+++ This_is_CodingTest/Greedy/practices/무지의 먹방 라이브.py
@@ -0,0 +1,24 @@
+import heapq
+
+
+def solution(food_times, k):
+    if sum(food_times) <= k:
+        return -1
+
+    q = []
+    for i in range(len(food_times)):
+        heapq.heappush(q, (food_times[i], i + 1))
+
+    sum_time = 0
+    pre_time = 0
+
+    food_cnt = len(food_times)
+
+    while sum_time + (q[0][0] - pre_time) * food_cnt <= k:
+        this_food_time = heapq.heappop(q)[0]
+        sum_time += (this_food_time - pre_time) * food_cnt
+        food_cnt -= 1
+        pre_time = this_food_time
+
+    result = sorted(q, key=lambda x: x[1])
+    return result[(k - sum_time) % food_cnt][1]
--- This_is_CodingTest/Greedy/practices/볼링공 고르기.py
+++ This_is_CodingTest/Greedy/practices/볼링공 고르기.py
@@ -2,6 +2,8 @@ n,m = map(int,input().split())
 
 data = list(map(int,input().split()))
 
+# solution 1
+
 result = n*(n-1) / 2
 
 print(result)
@@ -19,3 +21,21 @@ for i in overlap:
     result -= n*(n-1) / 2
 
 print(int(result))
+
+# solution 2
+
+n,m = map(int,input().split())
+
+data = list(map(int,input().split()))
+
+result=0
+
+arr = [0]*11
+for i in data:
+    arr[i] += 1
+
+for i in range(1,m+1):
+    n -= arr[i]
+    result += n * arr[i]
+
+print(result)
