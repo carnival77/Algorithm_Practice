68ce5c1802f0b62c54a0a0ff783b4f90ceeb0215
carnival77 <stam0325@gmail.com>
2021-06-17 17:46:31 +0900

    연산자 끼워넣기 이해

--- This_is_CodingTest/DFS,BFS/practices/연산자 끼워넣기.py
+++ This_is_CodingTest/DFS,BFS/practices/연산자 끼워넣기.py
@@ -26,37 +26,41 @@
 
 
 n = int(input())
-arr = list(map(int,input().split()))
-add, sub, mul, div = map(int,input().split())
+arr = list(map(int, input().split()))
+add, sub, mul, div = map(int, input().split())
 
 min_val = 1e9
 max_val = -1e9
 
-def dfs(index,now):
-    global min_val,max_val,n,add,sub,mul,div
+
+def dfs(index, now):
+    global min_val, max_val, n, add, sub, mul, div, temp
+    global count
 
     if index == n:
-        min_val = min(min_val,now)
-        max_val = max(max_val,now)
+        min_val = min(min_val, now)
+        max_val = max(max_val, now)
+
     else:
-        if add > 0 :
+        if add > 0:
             add -= 1
-            dfs(index+1, now+arr[index])
+            dfs(index + 1, now + arr[index])
             add += 1
-        if sub > 0 :
+        if sub > 0:
             sub -= 1
-            dfs(index+1, now-arr[index])
+            dfs(index + 1, now - arr[index])
             sub += 1
-        if mul > 0 :
+        if mul > 0:
             mul -= 1
-            dfs(index+1, now*arr[index])
+            dfs(index + 1, now * arr[index])
             mul += 1
-        if div > 0 :
+        if div > 0:
             div -= 1
-            dfs(index+1, int(now/arr[index]))
+            dfs(index + 1, int(now / arr[index]))
             div += 1
 
-dfs(1,arr[0])
+
+dfs(1, arr[0])
 
 print(max_val)
 print(min_val)
