d225679713d43f06f0ca5e34dacadb132c771eb8
carnival77 <stam0325@gmail.com>
2021-06-19 15:39:10 +0900

    46. 0619. solved 21. 인구 이동

--- This_is_CodingTest/DFS,BFS/practices/인구 이동.py
+++ This_is_CodingTest/DFS,BFS/practices/인구 이동.py
@@ -1,36 +1,75 @@
 from collections import deque
 
-N,L,R = map(int,input().split())
+n,l,r = map(int,input().split())
 
 board=[]
 
-for _ in range(N):
+for _ in range(n):
     board.append(list(map(int,input().split())))
 
-visi
+dy = [0,0,-1,1]
+dx = [-1,1,0,0]
 
-union = deque()
+day_cnt=0
 
-# 차이값 sub이 L,R 범위 내인 지 체크. 범위 내이면 True
-def between(sub):
-    if sub >= L and sub <= R:
-        return True
-    else:
-        return False
+# bfs 로 인구 이동 가능한 국가들을 연합으로 그룹 짓고, 같은 union_num을 부여한 뒤, 연합 내 인구 이동 실행.
+def process(x,y,union_num):
+    # 연합된 국가들
+    united=[]
+    united.append((x,y))
+    # 연합 국가들의 인구 수 합
+    sum = board[x][y]
+    # bfs 위한 연합 국가들 q
+    q = deque()
+    q.append((x,y))
+    # 현재 연합 번호 할당
+    union[x][y] = union_num
 
-# board 전체를 완전 탐색
-for x in range(N):
-    for y in range(N):
+    while q:
+        x,y = q.popleft()
 
-        # board 각 칸마다 상, 하, 좌, 우 살핀다
+        # 상하좌우
         for direction in range(4):
+            nx = x + dx[direction]
+            ny = y + dy[direction]
 
-            # 상
-            if direction == 0:
-                # 해당 방향의 칸이 맵을 벗어나 존재하지 않고, 아직 방문하지 않았을 때
-                if x-1 <= 0 and visited[]:
-                    # 해당 방향의 인접칸과 this 칸의 value 차이값을 계산하고, L <= 차이값 <= R 이라면, queue에 담아 연합을 이룬다.
-                    sub = board[x-1][y] - board[x][y]
-                    if between(sub):
+            # 상하좌우 방향의 해당 칸이 맵 내에 있고 아직 union에 가입되지 않았다면
+            if nx >=0 and ny >= 0 and nx < n and ny < n and union[nx][ny] == -1:
+                # 인구 차이가 l 이상 r 이하라면
+                if l <= abs(board[x][y] - board[nx][ny])  <= r:
+                    # 같은 연합이므로
+                    # q에 삽입
+                    q.append((nx,ny))
+                    # united에 삽입
+                    united.append((nx,ny))
+                    # 연합 번호 할당
+                    union[nx][ny] = union_num
 
-                        union.append()
+                    sum += board[nx][ny]
+
+    # 연합 내 국가 간 인구 이동
+    for x,y in united:
+        board[x][y] = sum // len(united)
+
+while True:
+    # 매일마다 연합 갱신. 아직 연합이 정해지지 않은 곳은 -1
+    union = [[-1] * n for _ in range(n)]
+    # 연합 개수(번호). 이것이 n*n 과 같게 되어 연합이 전혀 없고 각 나라가 개인팀이 되어야 인구 이동 종료
+    union_num=0
+    # board 전체 체크
+    for x in range(n):
+        for y in range(n):
+            # 만약 연합이 정해지지 않은 곳이면
+            if union[x][y] == -1:
+                # process 실행. bfs 로 인구 이동 가능한 국가들을 연합으로 그룹 짓고, 같은 union_num을 부여한 뒤, 연합 내 인구 이동 실행.
+                process(x,y,union_num)
+                # 연합 개수(번호) +1
+                union_num += 1
+
+    # 연합 개수(번호). 이것이 n*n 과 같게 되어 연합이 전혀 없고 각 나라가 개인팀이 되어야 인구 이동 종료
+    if union_num == n*n:
+        break
+    # 한 번의 인구 이동이 종료되면 1일 +
+    day_cnt += 1
+
+print(day_cnt)
--- This_is_CodingTest/temp.py
+++ This_is_CodingTest/temp.py
@@ -1,100 +1,66 @@
-from itertools import combinations
+from collections import deque
 
-n = int(input()) # 복도의 크기
-board = [] # 복도 정보 (N x N)
-teachers = [] # 모든 선생님 위치 정보
-spaces = [] # 모든 빈 공간 위치 정보
+# 땅의 크기(N), L, R 값을 입력받기
+n, l, r = map(int, input().split())
 
-for i in range(n):
-    board.append(list(input().split()))
-    for j in range(n):
-        # 선생님이 존재하는 위치 저장
-        if board[i][j] == 'T':
-            teachers.append((i, j))
-        # 장애물을 설치할 수 있는 (빈 공간) 위치 저장
-        if board[i][j] == 'X':
-            spaces.append((i, j))
+# 전체 나라의 정보(N x N)를 입력 받기
+graph = []
+for _ in range(n):
+    graph.append(list(map(int, input().split())))
 
-# 특정 방향으로 감시를 진행 (학생 발견: True, 학생 미발견: False)
-def watch(x, y, direction):
-    # 왼쪽 방향으로 감시
-    if direction == 0:
-        while y >= 0:
-            if board[x][y] == 'S': # 학생이 있는 경우
-                return True
-            if board[x][y] == 'O': # 장애물이 있는 경우
-                return False
-            y -= 1
-    # 오른쪽 방향으로 감시
-    if direction == 1:
-        while y < n:
-            if board[x][y] == 'S': # 학생이 있는 경우
-                return True
-            if board[x][y] == 'O': # 장애물이 있는 경우
-                return False
-            y += 1
-    # 위쪽 방향으로 감시
-    if direction == 2:
-        while x >= 0:
-            if board[x][y] == 'S': # 학생이 있는 경우
-                return True
-            if board[x][y] == 'O': # 장애물이 있는 경우
-                return False
-            x -= 1
-    # 아래쪽 방향으로 감시
-    if direction == 3:
-        while x < n:
-            if board[x][y] == 'S': # 학생이 있는 경우
-                return True
-            if board[x][y] == 'O': # 장애물이 있는 경우
-                return False
-            x += 1
-    return False
+dx = [-1, 0, 1, 0]
+dy = [0, -1, 0, 1]
 
-# 장애물 설치 이후에, 한 명이라도 학생이 감지되는지 검사
-def process():
-    # 모든 선생의 위치를 하나씩 확인
-
-    print("teachers")
-    print(teachers)
-
-    for x, y in teachers:
-
-        print("x,y")
-        print(x,y)
-
-        # 4가지 방향으로 학생을 감지할 수 있는지 확인
+# 특정 위치에서 출발하여 모든 연합을 체크한 뒤에 데이터 갱신
+def process(x, y, index):
+    # (x, y)의 위치와 연결된 나라(연합) 정보를 담는 리스트
+    united = []
+    united.append((x, y))
+    # 너비 우선 탐색 (BFS)을 위한 큐 라이브러리 사용
+    q = deque()
+    q.append((x, y))
+    union[x][y] = index # 현재 연합의 번호 할당
+    summary = graph[x][y] # 현재 연합의 전체 인구 수
+    count = 1 # 현재 연합의 국가 수
+    # 큐가 빌 때까지 반복(BFS)
+    while q:
+        x, y = q.popleft()
+        # 현재 위치에서 4가지 방향을 확인하며
         for i in range(4):
-            if watch(x, y, i):
+            nx = x + dx[i]
+            ny = y + dy[i]
+            # 바로 옆에 있는 나라를 확인하여
+            if 0 <= nx < n and 0 <= ny < n and union[nx][ny] == -1:
+                # 옆에 있는 나라와 인구 차이가 L명 이상, R명 이하라면
+                if l <= abs(graph[nx][ny] - graph[x][y]) <= r:
+                    q.append((nx, ny))
+                    # 연합에 추가하기
+                    union[nx][ny] = index
+                    summary += graph[nx][ny]
+                    count += 1
+                    united.append((nx, ny))
+    # 연합 국가끼리 인구를 분배
+    for i, j in united:
+        graph[i][j] = summary // count
 
-                return True
-    return False
+total_count = 0
 
-find = False # 학생이 한 명도 감지되지 않도록 설치할 수 있는지의 여부
-
-# 빈 공간에서 3개를 뽑는 모든 조합을 확인
-for data in combinations(spaces, 3):
-
-    print("data")
-    print(data)
-
-    # 장애물들을 설치해보기
-    for x, y in data:
-
-        print("x,y")
-        print(x,y)
-
-        board[x][y] = 'O'
-    # 학생이 한 명도 감지되지 않는 경우
-    if not process():
-        # 원하는 경우를 발견한 것임
-        find = True
+# 더 이상 인구 이동을 할 수 없을 때까지 반복
+while True:
+    union = [[-1] * n for _ in range(n)]
+    index = 0
+    for i in range(n):
+        for j in range(n):
+            if union[i][j] == -1: # 해당 나라가 아직 처리되지 않았다면
+                process(i, j, index)
+                print(index,end=' ')
+                index += 1
+    print(index)
+    # 모든 인구 이동이 끝난 경우
+    if index == n * n:
         break
-    # 설치된 장애물을 다시 없애기
-    for x, y in data:
-        board[x][y] = 'X'
+    print(total_count)
+    total_count += 1
 
-if find:
-    print('YES')
-else:
-    print('NO')
+# 인구 이동 횟수 출력
+print(total_count)
