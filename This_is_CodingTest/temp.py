45a137ce86d03ea119a8dc1410d775eac1c34da0
carnival77 <stam0325@gmail.com>
2021-06-20 17:05:50 +0900

    47. 0620. solved 22. 블록 이동 - kakao 2020

--- This_is_CodingTest/temp.py
+++ This_is_CodingTest/temp.py
@@ -1,66 +1,55 @@
 from collections import deque
 
-# 땅의 크기(N), L, R 값을 입력받기
-n, l, r = map(int, input().split())
+def get_next_pos(pos, board):
+    next_pos = [] # 반환 결과 (이동 가능한 위치들)
+    pos = list(pos) # 현재 위치 정보를 리스트로 변환 (집합 → 리스트)
+    pos1_x, pos1_y, pos2_x, pos2_y = pos[0][0], pos[0][1], pos[1][0], pos[1][1]
+    # (상, 하, 좌, 우)로 이동하는 경우에 대해서 처리
+    dx = [-1, 1, 0, 0]
+    dy = [0, 0, -1, 1]
+    for i in range(4):
+        pos1_next_x, pos1_next_y, pos2_next_x, pos2_next_y = pos1_x + dx[i], pos1_y + dy[i], pos2_x + dx[i], pos2_y + dy[i]
+        # 이동하고자 하는 두 칸이 모두 비어 있다면
+        if board[pos1_next_x][pos1_next_y] == 0 and board[pos2_next_x][pos2_next_y] == 0:
+            next_pos.append({(pos1_next_x, pos1_next_y), (pos2_next_x, pos2_next_y)})
+    # 현재 로봇이 가로로 놓여 있는 경우
+    if pos1_x == pos2_x:
+        for i in [-1, 1]: # 위쪽으로 회전하거나, 아래쪽으로 회전
+            if board[pos1_x + i][pos1_y] == 0 and board[pos2_x + i][pos2_y] == 0: # 위쪽 혹은 아래쪽 두 칸이 모두 비어 있다면
+                next_pos.append({(pos1_x, pos1_y), (pos1_x + i, pos1_y)})
+                next_pos.append({(pos2_x, pos2_y), (pos2_x + i, pos2_y)})
+    # 현재 로봇이 세로로 놓여 있는 경우
+    elif pos1_y == pos2_y:
+        for i in [-1, 1]: # 왼쪽으로 회전하거나, 오른쪽으로 회전
+            if board[pos1_x][pos1_y + i] == 0 and board[pos2_x][pos2_y + i] == 0: # 왼쪽 혹은 오른쪽 두 칸이 모두 비어 있다면
+                next_pos.append({(pos1_x, pos1_y), (pos1_x, pos1_y + i)})
+                next_pos.append({(pos2_x, pos2_y), (pos2_x, pos2_y + i)})
+    # 현재 위치에서 이동할 수 있는 위치를 반환
+    return next_pos
 
-# 전체 나라의 정보(N x N)를 입력 받기
-graph = []
-for _ in range(n):
-    graph.append(list(map(int, input().split())))
-
-dx = [-1, 0, 1, 0]
-dy = [0, -1, 0, 1]
-
-# 특정 위치에서 출발하여 모든 연합을 체크한 뒤에 데이터 갱신
-def process(x, y, index):
-    # (x, y)의 위치와 연결된 나라(연합) 정보를 담는 리스트
-    united = []
-    united.append((x, y))
-    # 너비 우선 탐색 (BFS)을 위한 큐 라이브러리 사용
-    q = deque()
-    q.append((x, y))
-    union[x][y] = index # 현재 연합의 번호 할당
-    summary = graph[x][y] # 현재 연합의 전체 인구 수
-    count = 1 # 현재 연합의 국가 수
-    # 큐가 빌 때까지 반복(BFS)
-    while q:
-        x, y = q.popleft()
-        # 현재 위치에서 4가지 방향을 확인하며
-        for i in range(4):
-            nx = x + dx[i]
-            ny = y + dy[i]
-            # 바로 옆에 있는 나라를 확인하여
-            if 0 <= nx < n and 0 <= ny < n and union[nx][ny] == -1:
-                # 옆에 있는 나라와 인구 차이가 L명 이상, R명 이하라면
-                if l <= abs(graph[nx][ny] - graph[x][y]) <= r:
-                    q.append((nx, ny))
-                    # 연합에 추가하기
-                    union[nx][ny] = index
-                    summary += graph[nx][ny]
-                    count += 1
-                    united.append((nx, ny))
-    # 연합 국가끼리 인구를 분배
-    for i, j in united:
-        graph[i][j] = summary // count
-
-total_count = 0
-
-# 더 이상 인구 이동을 할 수 없을 때까지 반복
-while True:
-    union = [[-1] * n for _ in range(n)]
-    index = 0
+def solution(board):
+    # 맵의 외곽에 벽을 두는 형태로 맵 변형
+    n = len(board)
+    new_board = [[1] * (n + 2) for _ in range(n + 2)]
     for i in range(n):
         for j in range(n):
-            if union[i][j] == -1: # 해당 나라가 아직 처리되지 않았다면
-                process(i, j, index)
-                print(index,end=' ')
-                index += 1
-    print(index)
-    # 모든 인구 이동이 끝난 경우
-    if index == n * n:
-        break
-    print(total_count)
-    total_count += 1
-
-# 인구 이동 횟수 출력
-print(total_count)
+            new_board[i + 1][j + 1] = board[i][j]
+    # 너비 우선 탐색(BFS) 수행
+    q = deque()
+    visited = []
+    pos = {(1, 1), (1, 2)} # 시작 위치 설정
+    q.append((pos, 0)) # 큐에 삽입한 뒤에
+    visited.append(pos) # 방문 처리
+    # 큐가 빌 때까지 반복
+    while q:
+        pos, cost = q.popleft()
+        # (n, n) 위치에 로봇이 도달했다면, 최단 거리이므로 반환
+        if (n, n) in pos:
+            return cost
+        # 현재 위치에서 이동할 수 있는 위치 확인
+        for next_pos in get_next_pos(pos, new_board):
+            # 아직 방문하지 않은 위치라면 큐에 삽입하고 방문 처리
+            if next_pos not in visited:
+                q.append((next_pos, cost + 1))
+                visited.append(next_pos)
+    return 0
