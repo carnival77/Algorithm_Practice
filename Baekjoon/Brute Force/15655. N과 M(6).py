54a22696c9237c374fbfd24fae8bc7fe63c6f737
carnival77 <stam0325@gmail.com>
2021-07-07 17:39:34 +0900

    70. 백준 코딩테스트 강의 - 기초편. 브루트포스. 15654. N과 M(5)

--- Baekjoon/Brute Force/15655. N과 M(6).py
+++ Baekjoon/Brute Force/15655. N과 M(6).py
@@ -1,27 +1,25 @@
 import sys
 n,m = map(int,input().split())
 
-used = [False] * n
-
+used = [False] * (n+1)
 result = [0]*m
 
 arr = list(map(int,input().split()))
-arr.sort()
 
 index=0
 
-def recursive(index,n,m):
+def recursive(index,start,n,m):
     if index==m:
         sys.stdout.write(' '.join(map(str,result))+'\n')
         return
 
-    for i in range(n):
+    for i in range(start,n):
         if used[i]:
             continue
         else:
             used[i] = True
-            result[index] = arr[i]
-            recursive(index+1,n,m)
+            result[index] = i
+            recursive(index+1,i+1,n,m)
             used[i] = False
 
-recursive(0,n,m)
+recursive(0,1,n,m)
