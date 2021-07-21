14cb93b451c1672865fc97bc863eb5efaeab5dac
carnival77 <stam0325@gmail.com>
2021-07-21 16:06:38 +0900

    83,84,85.백준 코딩테스트 강의 - 기초편.브루트포스.10972,3,4, 10819.이전순열,모든순열,차이를최대로

---
+++ CodingTest_C++/10819.차이를최대로.cpp
@@ -0,0 +1,27 @@
+#include<iostream>
+#include<algorithm>
+#include<vector>
+using namespace std;
+int cal(vector<int>& a,int n) {
+	int result = 0;
+	for (int i = 1; i < a.size(); i++) {
+		result += abs(a[i - 1] - a[i]);
+	}
+	return result;
+}
+int main() {
+	int n;
+	int ans=0;
+	cin >> n;
+	vector<int> a(n);
+	for (int i = 0; i < n; i++) {
+		cin >> a[i];
+	}
+	sort(a.begin(), a.end());
+	do {
+		int temp = cal(a,n);
+		ans = max(ans, temp);
+	} while (next_permutation(a.begin(), a.end()));
+	cout << ans << '\n';
+	return 0;
+}
--- CodingTest_C++/10972.다음순열.cpp
+++ CodingTest_C++/10972.다음순열.cpp
@@ -38,4 +38,3 @@ int main() {
 	cout << "\n";
 	return 0;
 }
-///
