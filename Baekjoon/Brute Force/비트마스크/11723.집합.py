7aaca31459b8f65ed70d15ffcf807ac3abeebd3c
carnival77 <stam0325@gmail.com>
2021-07-27 13:39:11 +0900

    89. 백준 코딩테스트 강의 - 기초편. 브루트포스-비트마스크. 1182. 부분수열의 합

--- Baekjoon/Brute Force/비트마스크/11723.집합.py
+++ Baekjoon/Brute Force/비트마스크/11723.집합.py
@@ -4,7 +4,7 @@ input = sys.stdin.readline
 m=int(input())
 s=0
 for _ in range(m):
-    op,*temp = input().split()
+    op,*temp = input().split() #
     if(len(temp)>0):
         x=int(temp[0])-1
     if op=='add':
