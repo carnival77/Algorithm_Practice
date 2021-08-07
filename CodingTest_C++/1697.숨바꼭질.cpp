a9c6e59d3559b91848ecea5edb9083235424cf9a
carnival77 <stam0325@gmail.com>
2021-08-07 18:12:22 +0900

    103. 백준 코딩테스트 강의 - 기초편. BFS. 13913. 숨바꼭질 4

--- CodingTest_C++/1697.숨바꼭질.cpp
+++ CodingTest_C++/1697.숨바꼭질.cpp
@@ -2,7 +2,7 @@
 //1. 행동 3가지(x+1,x-1,2x) 모두 1초씩 소요되므로 BFS 로 풀이 가능
 //2. 위치 = 정점. 이동 가능한 경우의 수 = 위치 간의 간선.
 //3. 시간복잡도 : 정점 1개(10만) + 간선 3개(30만) = 40만. O(40만). 2초 내에 풀이 가능.
-
+//4. 정점의 범위 : 0~10만. 그러므로 2*X 하는 순간이동을 고려하면 최대 탐색 범위는 20만
 
 #include<iostream>
 #include<queue>
@@ -15,7 +15,7 @@ int dist[200001];
 
 int main() {
 	memset(check, false, sizeof(check));
-	memset(dist, -1, sizeof(check));
+	memset(dist, -1, sizeof(dist));
 	int n, k;
 	cin >> n >> k;
 	int ans = 1e9;
