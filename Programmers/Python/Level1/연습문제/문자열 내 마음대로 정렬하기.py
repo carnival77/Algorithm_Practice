bd2d07adfaf6d384d7fd45bbdddf2b87b1a18392
carnival77 <stam0325@gmail.com>
2021-09-14 19:16:08 +0900

    121. 프로그래머스 풀이. 레벨 1 - 문자열 내 마음대로 정렬하기 - 딕셔너리 x. 람다 활용

--- Programmers/Python/Level1/연습문제/문자열 내 마음대로 정렬하기.py
+++ Programmers/Python/Level1/연습문제/문자열 내 마음대로 정렬하기.py
@@ -1,11 +1,14 @@
+# def solution(strings, n):
+#     strings = sorted(strings)
+#
+#     sdict = {0: strings[0]}
+#
+#     for i in range(1, len(strings)):
+#         sdict[i] = strings[i]
+#
+#     answer = list(dict(sorted(sdict.items(), key=lambda x: x[1][n])).values())
+#
+#     return answer
+
 def solution(strings, n):
-    strings = sorted(strings)
-
-    sdict = {0: strings[0]}
-
-    for i in range(1, len(strings)):
-        sdict[i] = strings[i]
-
-    answer = list(dict(sorted(sdict.items(), key=lambda x: x[1][n])).values())
-
-    return answer
+    return sorted(sorted(strings), key=lambda x : x[n])
