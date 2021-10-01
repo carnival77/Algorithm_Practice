71e6d92987532883f3e516f49542c2201996e2d4
carnival77 <stam0325@gmail.com>
2021-10-01 15:27:40 +0900

    review 17. 백준 코테 강의 기초편 - 브루트 포스 - N과 M - 15652.N과 M(4).py

--- Baekjoon/강의 기초편/Review/Brute Force/N과 M/15652.N과 M(4).py
+++ Baekjoon/강의 기초편/Review/Brute Force/N과 M/15652.N과 M(4).py
@@ -1,4 +1,4 @@
-# 조합 문제. 조합 : N개의 수 중 M개를 순서 상관 하고 중복 허용하지 않고 뽑는다.
+# 중복순열 문제. 조합 : N개의 수 중 M개를 순서 상관 없이 중복 허용하여 뽑는다.
 # 추가 조건 : 오름차순
 # 구하는 것 : 어떤 위치에 이전에 사용되지 않은 어떤 수가 올 것인가
 # 변하는 것 : used, 위치, 수
@@ -17,9 +17,9 @@ def dfs(index,start):
 
     # 첫 번째 자리에 1 초과의 수가 올 수 있는 이유 : index =0 일 때 이 for 루프가 1~(n+1)까지 돌고 있으니까
     for num in range(start,n+1):
-        # 이전에 사용된 숫자라면 무시
-        if used[num]:
-            continue
+        # 이전에 사용된 숫자라도 허용. 중복 허용 O
+        # if used[num]:
+        #     continue
         used[num]=True
         arr[index]=num
         dfs(index+1,num+1)
