a30f16a39c99b04dbc80ec16b2fdc1719fda1bbe
carnival77 <stam0325@gmail.com>
2021-10-04 22:02:01 +0900

    146. 백준 코딩테스트 강의 - 연습편. 1182.부분수열의 합.py

---
+++ Baekjoon/강의 연습편/브루트 포스/재귀/1182.부분수열의 합.py
@@ -0,0 +1,46 @@
+# from itertools import combinations
+
+n,s = map(int,input().split())
+nums=list(map(int,input().split()))
+
+answer=0
+# for i in range(1,n):
+#     for j in combinations(arr,i):
+#         if sum(j) == s:
+#             answer+=1
+
+# solution 1
+def dfs(index,arr,m):
+    global answer
+    # 정답을 찾은 경우
+    if len(arr) == m:
+        if sum(arr) == s:
+            answer+=1
+        return
+    # 불가능한 경우
+    if index==n:
+        return
+
+    dfs(index+1,arr+[nums[index]],m)
+    dfs(index + 1, arr, m)
+
+for i in range(1,n+1):
+    dfs(0,[],i)
+
+print(answer)
+
+# solution 2
+answer=0
+def go(index,m):
+    global answer
+    if index==n:
+        if m==s:
+            answer+=1
+        return
+    go(index+1,m+nums[index])
+    go(index+1,m)
+
+go(0,0)
+if s==0:
+    answer-=1
+print(answer)
