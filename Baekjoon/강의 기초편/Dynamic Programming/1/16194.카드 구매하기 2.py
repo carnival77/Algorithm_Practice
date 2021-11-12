5e7da011e22f0578faee3e106205bc12309a3a38
carnival77 <stam0325@gmail.com>
2021-11-12 21:40:38 +0900

    191. 백준 기초편. 다이나믹 프로그래밍-1. 16194.카드 구매하기 2.py

---
+++ Baekjoon/강의 기초편/Dynamic Programming/1/16194.카드 구매하기 2.py
@@ -0,0 +1,11 @@
+n=int(input())
+
+d=[10000*1000]*(n+1)
+d[0]=0
+a=[0] + list(map(int,input().split()))
+
+for i in range(1,n+1):
+    for j in range(1,i+1):
+        d[i]=min(d[i],d[i-j]+a[j])
+
+print(d[n])
