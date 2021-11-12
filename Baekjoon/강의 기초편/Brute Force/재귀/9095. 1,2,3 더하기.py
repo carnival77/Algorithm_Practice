36adde43b840a3195b0cfdeeca3d3dce4a6faeba
carnival77 <stam0325@gmail.com>
2021-11-12 17:41:58 +0900

    9095. 1,2,3 더하기.py 복습

--- Baekjoon/강의 기초편/Brute Force/재귀/9095. 1,2,3 더하기.py
+++ Baekjoon/강의 기초편/Brute Force/재귀/9095. 1,2,3 더하기.py
@@ -1,14 +1,32 @@
+# t = int(input())
+#
+# def recur(cnt,sum,n):
+#     if sum>n : return 0
+#     if sum == n: return 1
+#     now=0
+#     for i in range(1,4):
+#         now += recur(cnt+1,sum+i,n)
+#     return now
+#
+# for i in range(t):
+#     n=int(input())
+#     print(recur(0,0,n))
+
+
+
+
 t = int(input())
 
-def recur(cnt,sum,n):
-    if sum>n : return 0
-    if sum == n: return 1
-    now=0
+def dfs(sum,cnt,n):
+    if sum==n:
+        return 1
+    if sum>n:
+        return 0
+    answer = 0
     for i in range(1,4):
-        now += recur(cnt+1,sum+i,n)
-    return now
+        answer += dfs(sum+i,cnt+1,n)
+
+    return answer
 
 for i in range(t):
-    n=int(input())
-    print(recur(0,0,n))
-
+    print(dfs(0,0,int(input())))
