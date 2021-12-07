18ab13cbe056e82ed1bf2f7c19d1762c89e10d0b
carnival77 <stam0325@gmail.com>
2021-12-07 12:07:06 +0900

    201. 프로그래머스 - 레벨3.네트워크.py - 해결

--- Programmers/Python/고득점 Kit/DFS,BFS/레벨3.네트워크.py
+++ Programmers/Python/고득점 Kit/DFS,BFS/레벨3.네트워크.py
@@ -1,35 +1,44 @@
 from collections import deque
 
-temp=[]
+result=[]
 matrix=[]
 visited=[]
 
 def bfs(s,n):
+    global result
     q=deque()
     q.append(s)
     visited[s] = True
 
+    if result[s] == -1:
+        result[s]=s
+
     while q:
         v=q.popleft()
         for i in range(n):
-            if visited[i] == False and matrix[s][i] == 1:
+            if visited[i] == False and matrix[v][i] == 1:
                 visited[i]=True
                 q.append(i)
-                temp[s][i] = s
+                result[i] = s
 
 def solution(n, computers):
-    global temp, visited,matrix
+    global temp, visited,matrix,result
     answer = 0
 
     matrix=computers
 
+    result = [-1] * n
     visited = [False] * n
-    temp = [[100] * n for _ in range(n)]
 
     for i in range(n):
         bfs(i,n)
 
-    for i in range(n):
-        print(temp[i])
+    # print(result)
 
-    return answer
+    return len(set(result))
+
+n=3
+computers = 	[[1, 1, 0], [1, 1, 0], [0, 0, 1]]
+# computers = 		[[1, 1, 0], [1, 1, 1], [0, 1, 1]]
+
+print(solution(n,computers))
