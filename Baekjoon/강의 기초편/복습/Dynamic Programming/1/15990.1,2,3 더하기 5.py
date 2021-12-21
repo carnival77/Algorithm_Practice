e3e8bb6e22f71ec023476b71050ec1b318393515
carnival77 <stam0325@gmail.com>
2021-12-21 23:49:55 +0900

    review. 백준 기초편. 다이나믹 프로그래밍-1. 15990.1,2,3 더하기 5.py

---
+++ Baekjoon/강의 기초편/복습/Dynamic Programming/1/15990.1,2,3 더하기 5.py
@@ -0,0 +1,28 @@
+t=int(input())
+
+MAX = 100000
+
+mod = 1000000009
+
+d=[[0] * 4 for _ in range(MAX+1)]
+
+for i in range(1,MAX+1):
+    if i>=1:
+        d[i][1] = d[i-1][2] + d[i-1][3]
+        if i==1:
+            d[i][1]=1
+    if i>=2:
+        d[i][2] = d[i-2][1] + d[i-2][3]
+        if i==2:
+            d[i][2]=1
+    if i>=3:
+        d[i][3] = d[i-3][1] + d[i-3][2]
+        if i==3:
+            d[i][3]=1
+    d[i][1] %= mod
+    d[i][2] %= mod
+    d[i][3] %= mod
+
+for _ in range(t):
+    n=int(input())
+    print(sum(d[n])%mod)
