c465fbbdb0e9efb2399bd9641dfebd4491ed5090
carnival77 <stam0325@gmail.com>
2021-12-30 15:23:51 +0900

    211. 백준 기초편 - DP - 10844.쉬운 계단 수.py

--- Baekjoon/강의 기초편/Dynamic Programming/1/10844.쉬운 계단 수.py
+++ Baekjoon/강의 기초편/Dynamic Programming/1/10844.쉬운 계단 수.py
@@ -1,16 +1,30 @@
-d = [[0]*10 for _ in range(100+1)]
-mod = 1000000000
-n = int(input())
-for i in range(1, 10):
-    d[1][i] = 1
-for i in range(2, n+1):
+n=int(input())
+
+d=[[0] * 10 for _ in range(101)]
+# d[i][j] : 길이가 i이고 마지막 숫자가 j인 계단수의 개수
+
+mod=1000000000
+
+for i in range(1,n+1):
     for j in range(10):
-        d[i][j] = 0
-        if j-1 >= 0:
-            d[i][j] += d[i-1][j-1]
-        if j+1 <= 9:
-            d[i][j] += d[i-1][j+1]
-        d[i][j] %= mod
-ans = sum(d[n]) % mod
+        if i==1:
+            if j==0:
+                d[i][j]=0
+            elif 1<=j<=9:
+                d[i][j]=1
+        else:
+            if j==0:
+                d[i][j] = d[i-1][j+1]
+            elif j==9:
+                d[i][j] = d[i-1][j-1]
+            else:
+                d[i][j] = d[i-1][j-1] + d[i-1][j+1]
+        d[i][j]%=mod
+
+ans=0
+for j in range(10):
+    ans+=d[n][j]
+
+ans%=mod
+
 print(ans)
-#
