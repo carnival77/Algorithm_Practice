b670f8a59d0fac7d578b7cd329533509bc82fe5b
carnival77 <stam0325@gmail.com>
2022-02-01 23:52:37 +0900

    review. 백준 기초편. 다이나믹 프로그래밍-1. 11053.가장 긴 증가하는 부분 수열.py

--- Baekjoon/강의 기초편/Dynamic Programming/1/11053.가장 긴 증가하는 부분 수열.py
+++ Baekjoon/강의 기초편/Dynamic Programming/1/11053.가장 긴 증가하는 부분 수열.py
@@ -1,9 +1,50 @@
-n = int(input())
-a = list(map(int,input().split()))
-d = [0]*n
+# n = int(input())
+# a = list(map(int,input().split()))
+# d = [0]*n
+# for i in range(n):
+#     d[i] = 1
+#     for j in range(i):
+#         if a[j] < a[i] and d[j]+1 > d[i]:
+#             d[i] = d[j]+1
+# print(max(d))
+
+
+
+
+
+
+
+
+
+
++
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+n=int(input())
+a=list(map(int,input().split()))
+
+dp=[0]*n
+
 for i in range(n):
-    d[i] = 1
+    dp[i]=1
     for j in range(i):
-        if a[j] < a[i] and d[j]+1 > d[i]:
-            d[i] = d[j]+1
-print(max(d))
+        if a[j]<a[i] and dp[j]+1 > dp[i]:
+            dp[i]=dp[j]+1
+
+print(max(dp))
+
