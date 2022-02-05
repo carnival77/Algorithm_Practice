987b82496fee25e9f98a0f5adbee2f972281bf8c
carnival77 <stam0325@gmail.com>
2022-02-05 23:11:24 +0900

    review. 백준 기초편. 다이나믹 프로그래밍-1. 1912.연속합.py

--- Baekjoon/강의 기초편/Dynamic Programming/1/1912.연속합.py
+++ Baekjoon/강의 기초편/Dynamic Programming/1/1912.연속합.py
@@ -1,10 +1,24 @@
-n = int(input())
-a = list(map(int,input().split()))
-d = [0]*n
-for i in range(n):
-    d[i] = a[i]
-    if i == 0:
-        continue
+# n = int(input())
+# a = list(map(int,input().split()))
+# d = [0]*n
+# for i in range(n):
+#     d[i] = a[i]
+#     if i == 0:
+#         continue
+#     if d[i] < d[i-1] + a[i]:
+#         d[i] = d[i-1] + a[i]
+# print(max(d))
+
+
+n=int(input())
+a=list(map(int,input().split()))
+d=[0]*n
+
+d[0]=a[0]
+
+for i in range(1,n):
+    d[i]=a[i]
     if d[i] < d[i-1] + a[i]:
-        d[i] = d[i-1] + a[i]
+        d[i]=d[i-1]+a[i]
+
 print(max(d))
