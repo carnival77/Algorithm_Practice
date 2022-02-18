213345d574b382136fee6d35d7f64dec45ebb8b7
carnival77 <stam0325@gmail.com>
2022-02-18 23:28:22 +0900

    233. 백준 기초편. 다이나믹 프로그래밍-2. 11055.가장 큰 증가 부분 수열.py

--- Baekjoon/강의 기초편/Dynamic Programming/1/11053.가장 긴 증가하는 부분 수열.py
+++ Baekjoon/강의 기초편/Dynamic Programming/1/11053.가장 긴 증가하는 부분 수열.py
@@ -20,7 +20,11 @@
 n=int(input())
 a=list(map(int,input().split()))
 
+# dp[i] = a[1]...a[i] 까지의 수열이 있을 때, a[i]를 마지막으로 하는 가장 긴 증가하는 부분수열의 길이
 dp=[0]*n
+
+# 조건에 맞으려면, a[1] ... a[j],a[i] 수열이 있으면, j<i , a[j] < a[i]. 이어야 한다.
+# a[j]<a[i] 인 상태에서, d[j] 에 a[i] 1개를 추가한 길이가 d[i]보다 크면, d[i]는 d[j] 에 a[i]을 더한 길이로 변경해준다.
 
 for i in range(n):
     dp[i]=1
