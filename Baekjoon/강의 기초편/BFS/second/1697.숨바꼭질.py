640532d9c19afd6c542a2af7ecab1896ff1fd27f
carnival77 <stam0325@gmail.com>
2022-03-23 22:38:19 +0900

    review. 백준 기초편 - BFS 1697.숨바꼭질.py

--- Baekjoon/강의 기초편/BFS/second/1697.숨바꼭질.py
+++ Baekjoon/강의 기초편/BFS/second/1697.숨바꼭질.py
@@ -1,3 +1,28 @@
+# from collections import deque
+#
+# MAX = 200000
+#
+# n,k = map(int,input().split())
+#
+# check=[False]*(MAX+1)
+# dist=[-1]*(MAX+1)
+#
+# check[n]=True
+# dist[n]=0
+#
+# q=deque()
+# q.append(n)
+#
+# while q:
+#     now = q.popleft()
+#     for next in [now+1,now-1,now*2]:
+#         if 0<=next<=MAX and check[next] == False:
+#             q.append(next)
+#             check[next]=True
+#             dist[next] = dist[now]+1
+#
+# print(dist[k])
+
 from collections import deque
 
 MAX = 200000
