d6ce914fa96d27703c1436688b4ad80e0914fc72
carnival77 <stam0325@gmail.com>
2022-03-27 16:36:50 +0900

    review. 백준 연습편 - 브루트 포스 - 비트마스크 - 12100.2048(Easy).py

--- Baekjoon/강의/강의 연습편/브루트 포스/비트마스크/first/12100.2048(Easy).py
+++ Baekjoon/강의/강의 연습편/브루트 포스/비트마스크/first/12100.2048(Easy).py
@@ -1,8 +1,6 @@
-dx = [0, 0, 1, -1]
-dy = [1, -1, 0, 0]
 LIMIT = 5
 
-
+# 비트 연산 사용하여 정수 k를 (2진법으로 나타난 후) 4진법으로 변환하여 반환
 def gen(k):
     a = [0] * LIMIT
     for i in range(LIMIT):
@@ -10,11 +8,12 @@ def gen(k):
         k >>= 2
     return a
 
-
+# 보드 a를 그 방향(dirs)으로 이동 시켜서, 최댓값을 반환한다.
 def check(a, dirs):
     n = len(a)
     d = [row[:] for row in a]
 
+    # 0: down, 1: up, 2: left, 3: right
     for dir in dirs:
         ok = False
         merged = [[False] * n for _ in range(n)]
@@ -95,7 +94,10 @@ def check(a, dirs):
 n = int(input())
 a = [list(map(int, input().split())) for _ in range(n)]
 ans = 0
+# 모든 이동 방법을 만든다. 여기서 k는 정수(비트마스크)이다.
 for k in range(1 << (LIMIT * 2)):
+    # gen 함수를 통해, 정수 k를 0,1,2,3 으로 이뤄진 4진법 수로 바꾼다. 0,1,2,3은 위,아래,오른쪽,왼쪽 4가지 방향을 가리킨다.
+    # 즉, dirs 는 10번 안에 가능한 모든 4가지 방향들의 집합이다.
     dirs = gen(k)
     cur = check(a, dirs)
     if ans < cur:
