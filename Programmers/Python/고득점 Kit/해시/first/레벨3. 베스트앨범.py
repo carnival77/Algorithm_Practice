9a46ac0660274873d73323d8b52f10130607591a
carnival77 <stam0325@gmail.com>
2022-04-15 14:16:00 +0900

    디렉터리 정리

--- Programmers/Python/고득점 Kit/해시/first/레벨3. 베스트앨범.py
+++ Programmers/Python/고득점 Kit/해시/first/레벨3. 베스트앨범.py
@@ -60,24 +60,28 @@ def solution(genres, plays):
 
     return answer
 
-def solution2(genres, plays):
-    genres_dict = {}
-    genres_list = []
-    for i in range(len(genres)):
-        if genres[i] not in genres_dict:
-            genres_dict[genres[i]] = []
-        genres_dict[genres[i]].append([i, plays[i]])
-
-    for g in genres_dict:
-        genres_dict[g].sort(key=lambda x: x[1], reverse=True)
-        genres_list.append([g, sum([play for _, play in genres_dict[g]])])
-
-    genres_list.sort(key=lambda x: x[1], reverse=True)
+def solution3(genres, plays):
     answer = []
-    for g, _ in genres_list:
-        answer.extend([x[0] for x in genres_dict[g][:2]])
-    return answer
 
+    dic1 = {}
+    dic2 = {}
+
+    for i, (g, p) in enumerate(zip(genres, plays)):
+        if g not in dic1:
+            dic1[g] = [(i, p)]
+        else:
+            dic1[g].append((i, p))
+
+        if g not in dic2:
+            dic2[g] = p
+        else:
+            dic2[g] += p
+
+    for (k, v) in sorted(dic2.items(), key=lambda x:x[1], reverse=True):
+        for (i, p) in sorted(dic1[k], key=lambda x:x[1], reverse=True)[:2]:
+            answer.append(i)
+
+    return answer
 
 genres = ["classic", "pop", "classic", "classic", "pop"]
 plays = [500, 600, 150, 800, 2500]
