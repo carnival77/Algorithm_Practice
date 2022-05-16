7f71c5957721d01b89d8e28405dc43e2e74d96f1
carnival77 <stam0325@gmail.com>
2022-05-16 23:59:38 +0900

    디렉터리 업데이트

--- 유형별 대표 예제/해시/레벨3. 베스트앨범.py
+++ 유형별 대표 예제/해시/레벨3. 베스트앨범.py
@@ -60,25 +60,6 @@ def solution(genres, plays):
 
     return answer
 
-def solution2(genres, plays):
-    genres_dict = {}
-    genres_list = []
-    for i in range(len(genres)):
-        if genres[i] not in genres_dict:
-            genres_dict[genres[i]] = []
-        genres_dict[genres[i]].append([i, plays[i]])
-
-    for g in genres_dict:
-        genres_dict[g].sort(key=lambda x: x[1], reverse=True)
-        genres_list.append([g, sum([play for _, play in genres_dict[g]])])
-
-    genres_list.sort(key=lambda x: x[1], reverse=True)
-    answer = []
-    for g, _ in genres_list:
-        answer.extend([x[0] for x in genres_dict[g][:2]])
-    return answer
-
-
 genres = ["classic", "pop", "classic", "classic", "pop"]
 plays = [500, 600, 150, 800, 2500]
 
