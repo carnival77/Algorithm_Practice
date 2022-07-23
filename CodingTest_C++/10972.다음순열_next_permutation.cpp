1f661d06fe5cab4f99856f8c0074cecfceb7bf48
carnival77 <stam0325@gmail.com>
2022-07-23 15:18:46 +0900

    review. 백준 기초편 - 브루트포스 - 순열. 10972.다음순열_next_permutation.cpp

--- CodingTest_C++/10972.다음순열_next_permutation.cpp
+++ CodingTest_C++/10972.다음순열_next_permutation.cpp
@@ -18,5 +18,29 @@ int main() {
 		cout << "-1";
 	}
 	cout << '\n';
-		return 0;
+	return 0;
 }
+
+
+//#include<iostream>
+//#include<algorithm>
+//#include<vector>
+//using namespace std;
+//int main() {
+//	int n;
+//	cin >> n;
+//	vector<int> a(n);
+//	for (int i = 0; i < n; i++) {
+//		cin >> a[i];
+//	}
+//	if (next_permutation(a.begin(), a.end())) {
+//		for (int i = 0; i < n; i++) {
+//			cout << a[i] << ' ';
+//		}
+//	}
+//	else {
+//		cout << "-1";
+//	}
+//	cout << '\n';
+//		return 0;
+//}
