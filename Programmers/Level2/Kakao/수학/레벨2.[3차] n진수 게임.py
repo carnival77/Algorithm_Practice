b6c58c7fb47d0c9a9b492cc81d5d5905eb2add44
carnival77 <stam0325@gmail.com>
2022-07-27 16:25:35 +0900

    299. 프로그래머스 - 카카오. 레벨2.[3차] n진수 게임.py

---
+++ Programmers/Level2/Kakao/수학/레벨2.[3차] n진수 게임.py
@@ -0,0 +1,33 @@
+﻿import string
+tmp=string.digits+string.ascii_uppercase
+def convert(num,base):
+    q,r=divmod(num,base)
+    if q==0:
+        return tmp[r]
+    else:
+        return convert(q,base)+tmp[r]
+
+def solution(n, t, m, p):
+    ans = ''
+    all=''
+    cnt=1
+    num = 0
+    while len(ans)<t:
+        inx=(p-1)+m*(cnt-1)
+        if inx>=len(all):
+            all+=convert(num,n)
+            num += 1
+        else:
+            ans+=all[inx]
+            cnt+=1
+
+    return ans
+
+# n=2
+n=16
+# t=4
+t=16
+m=2
+# p=1
+p=2
+print(solution(n,t,m,p))
