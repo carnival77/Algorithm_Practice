9f9acf3aa657d47aa68468cc0f37e7bc0d02f40a
carnival77 <stam0325@gmail.com>
2022-08-07 23:00:00 +0900

    367. 백준 유형별 풀이 - 이진 탐색 - 2470.두 용액.py

---
+++ Baekjoon/일반/이분탐색/2470.두 용액.py
@@ -0,0 +1,31 @@
+import sys
+n=int(input())
+a=list(map(int,input().split()))
+res=sys.maxsize
+ans=[]
+a.sort()
+
+for i in range(n-1):
+    cur=a[i]
+
+    start=i+1
+    end=n-1
+
+    while start<=end:
+        mid=(start+end)//2
+        s=cur+a[mid]
+
+        if abs(s)<res:
+            res=abs(s)
+            ans=[cur,a[mid]]
+            if res==0:
+                break
+                print(" ".join(map(str,ans)))
+                sys.exit(0)
+
+        if s<0:
+            start=mid+1
+        else:
+            end=mid-1
+
+print(" ".join(map(str,ans)))
