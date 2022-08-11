68bc3952c6d46efff496e83d65088a4442500294
carnival77 <stam0325@gmail.com>
2022-08-11 15:18:02 +0900

    311. 프로그래머스 - 레벨2.모음 사전.py

---
+++ Programmers/Level2/완전 탐색/레벨2.모음 사전.py
@@ -0,0 +1,24 @@
+﻿from itertools import product
+
+def solution(word):
+    arr= {'A', 'E', 'I', 'O', 'U'}
+    words='AEIOU'
+    for i in range(4):
+        for prd in product(arr,words):
+            arr.add("".join(list(prd)))
+    result = list(arr)
+    result.sort()
+    answer=result.index(word)+1
+
+    return answer
+
+from itertools import product
+
+solution = lambda word: sorted(["".join(c) for i in range(5) for c in product("AEIOU", repeat=i+1)]).index(word) + 1
+
+# word="AAAAE"
+# word="AAAE"
+# word="I"
+word="EIO"
+print(solution(word))
+
