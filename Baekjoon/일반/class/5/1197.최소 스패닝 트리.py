ceb584b70a98084202d7c0d0cbf586efcc3583f6
carnival77 <stam0325@gmail.com>
2022-08-20 23:00:00 +0900

    357. 백준 class 5 - 그래프이론.최소 스패닝 트리 - 1197.최소 스패닝 트리.py

---
+++ Baekjoon/일반/class/5/1197.최소 스패닝 트리.py
@@ -0,0 +1,34 @@
+v,e=map(int,input().split())
+parents=[0]*(v+1)
+edges=[]
+ans=0
+
+for i in range(1,v+1):
+    parents[i]=i
+
+for _ in range(e):
+    a,b,cost=map(int,input().split())
+    edges.append((cost,a,b))
+
+edges.sort()
+
+def find_parent(parents,x):
+    if parents[x]!=x:
+        parents[x]=find_parent(parents,parents[x])
+    return parents[x]
+
+def union_parent(parents,a,b):
+    a=find_parent(parents,a)
+    b=find_parent(parents,b)
+    if a<b:
+        parents[b]=a
+    else:
+        parents[a]=b
+
+for edge in edges:
+    cost,a,b=edge
+    if find_parent(parents,a)!=find_parent(parents,b):
+        union_parent(parents,a,b)
+        ans+=cost
+
+print(ans)
