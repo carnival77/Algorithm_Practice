d943d2cc803fbb2195a27b352ba0eb9aaf59a3a3
carnival77 <stam0325@gmail.com>
2022-09-04 23:00:00 +0900

    review. 백준 강의 기초편 - DP - 11054.가장 긴 바이토닉 부분 수열.py

--- Baekjoon/강의/강의 기초편/Dynamic Programming/2/second/11054.가장 긴 바이토닉 부분 수열.py
+++ Baekjoon/강의/강의 기초편/Dynamic Programming/2/second/11054.가장 긴 바이토닉 부분 수열.py
@@ -1,64 +1,25 @@
-# solution 1.
-# dp1[i] = a[i]에서 끝나는 가장 긴 증가하는 부분 수열의 길이와,
-# dp2[i] = a[i]부터 시작하는 가장 긴 감소하는 부분 수열의 길이 찾은 다음,
-# dp1[i] + dp2[i] - 1 한 값을 찾는다. ( 중복이 되기 때문 )
+# 가장 긴 바이토닉 부분 수열의 길이 = 가장 긴 증가하는 부분 수열의 길이(d) + 가장 긴 감소하는 부분 수열의 길이(d2) - 1
+# -> d[i] : a[i]까지의 수열 중 가장 긴 증가하는 부분 수열의 길이, d2[i] : a[i]부터의 수열 중 가장 긴 감소하는 부분 수열의 길이
+# ans=max(d[i]+d2[i]-1)
 
 n=int(input())
 a=list(map(int,input().split()))
 
-dp1=[0]*n
-dp2=[0]*n
-dp=[0]*n
+d=[1]*n # d[i] : a[i]까지의 수열 중 가장 긴 증가하는 부분 수열의 길이
 
-# a[i]에서 끝나는 가장 긴 증가하는 부분 수열의 길이
 for i in range(n):
-    dp1[i]=1
     for j in range(i):
         if a[j]<a[i]:
-            dp1[i]=max(dp1[i],dp1[j]+1)
-        # if a[j] < a[i] and dp1[j]+1 > dp1[i]:
-        #     dp1[i]=dp1[j]+1
+            d[i]=max(d[i],d[j]+1)
 
-# a[i]부터 시작하는 가장 긴 감소하는 부분 수열의 길이
+d2=[1]*n # d2[i] : a[i]부터의 수열 중 가장 긴 감소하는 부분 수열의 길이
+
 for i in range(n-1,-1,-1):
-    dp2[i]=1
-    for j in range(i+1,n):
+    for j in range(n-1,i,-1):
         if a[j]<a[i]:
-            dp2[i]=max(dp2[i],dp2[j]+1)
-        # if a[j]>a[i] and dp2[j]+1 > dp2[i]:
-        #     dp2[i]=dp2[j]+1
+            d2[i]=max(d2[i],d2[j]+1)
 
+ans=0
 for i in range(n):
-    dp[i]=dp1[i]+dp2[i]-1
-
-# print(dp1)
-#
-# print(dp2)
-#
-# print(dp)
-
-print(max(dp))
-
-# solution 2.
-# dp1[i] = a[i]에서 끝나는 가장 긴 증가하는 부분 수열의 길이와,
-# dp2[i] = a를 거꾸로 나열한 reversed_a 배열의 a[i]에서 끝나는 가장 긴 증가하는 부분 수열의 길이를 찾은 다음,
-# dp1[i] + reversed_dp2[i] - 1 한 값을 찾는다. ( 중복이 되기 때문 )
-# reversed_dp2[i] 인 이유는 dp2[i] 를 reverse 해야 a[i]부터 시작하는 가장 긴 감소하는 부분 수열의 길이가 되기 때문
-
-reversed_a = a[::-1]
-
-for i in range(n):
-    dp1[i]=1
-    dp2[i]=1
-    for j in range(i):
-        if a[j]<a[i]:
-            dp1[i]=max(dp1[i],dp1[j]+1)
-        if reversed_a[j] < reversed_a[i]:
-            dp2[i]=max(dp2[i],dp2[j]+1)
-
-reversed_dp2 = dp2[::-1]
-
-for i in range(n):
-    dp[i]=dp1[i]+reversed_dp2[i]-1
-
-print(max(dp))
+    ans=max(ans,d[i]+d2[i]-1)
+print(ans)
