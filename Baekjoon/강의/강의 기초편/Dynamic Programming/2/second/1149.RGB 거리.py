25929609ce6b2864dd1a4e34e522663d4bc3332e
carnival77 <stam0325@gmail.com>
2022-09-07 23:00:00 +0900

    review. 백준 강의 기초편 - DP - 1149.RGB 거리.py

--- Baekjoon/강의/강의 기초편/Dynamic Programming/2/second/1149.RGB 거리.py
+++ Baekjoon/강의/강의 기초편/Dynamic Programming/2/second/1149.RGB 거리.py
@@ -1,14 +1,16 @@
-# 해설 : https://carnival.tistory.com/69
-
 n=int(input())
-
-a = [[0,0,0]] + [list(map(int,input().split()))for _ in range(n)]
-
-d=[[0,0,0] for _ in range(n+1)]
-
+a=[[0]*3 for _ in range(n+1)]
 for i in range(1,n+1):
-    d[i][0] = min(d[i-1][1],d[i-1][2])+a[i][0]
-    d[i][1] = min(d[i-1][0],d[i-1][2])+a[i][1]
-    d[i][2] = min(d[i-1][0],d[i-1][1])+a[i][2]
+    data=list(map(int,input().split()))
+    a[i]=data
+d=[[0]*3 for _ in range(n+1)]
+d[1][0]=a[1][0]
+d[1][1]=a[1][1]
+d[1][2]=a[1][2]
 
-print(min(d[n][0],d[n][1],d[n][2]))
+for i in range(2,n+1):
+    d[i][0]=a[i][0]+min(d[i-1][1],d[i-1][2])
+    d[i][1]=a[i][1]+min(d[i-1][0],d[i-1][2])
+    d[i][2]=a[i][2]+min(d[i-1][0],d[i-1][1])
+
+print(min(d[n]))
