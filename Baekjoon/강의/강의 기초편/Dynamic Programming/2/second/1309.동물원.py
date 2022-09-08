8deeab4674caf1cdaa4db783bba870611e866c59
carnival77 <stam0325@gmail.com>
2022-09-08 23:00:00 +0900

    review. 백준 강의 기초편 - DP - 1309.동물원.py

--- Baekjoon/강의/강의 기초편/Dynamic Programming/2/second/1309.동물원.py
+++ Baekjoon/강의/강의 기초편/Dynamic Programming/2/second/1309.동물원.py
@@ -1,20 +1,17 @@
-# 해설 : https://carnival.tistory.com/manage/posts/
-
 n=int(input())
-mod = 9901
-
-d=[[0] * 3 for _ in range(n+1)]
-
-d[1][0] = 1
-d[1][1] = 1
-d[1][2] = 1
+d=[[0]*3 for _ in range((n+1))] # 2xn의 배열에 사자를 배치하는 경우의 수.
+# d[n][0],d[n][1],d[n][1] : 2xn의 배열의 n번째 줄의 왼쪽, 오른쪽에 사자를 배치하거나, 배치하지 않는 경우의 수
+mod=9901
+d[1][0]=1
+d[1][1]=1
+d[1][2]=1
 
 for i in range(2,n+1):
-    d[i][0] = d[i-1][0] + d[i-1][1] + d[i-1][2]
-    d[i][0] %= mod
-    d[i][1] = d[i-1][0] + d[i-1][2]
-    d[i][1] %= mod
-    d[i][2] = d[i-1][0] + d[i-1][1]
-    d[i][2] %= mod
+    d[i][0]=d[i-1][1]+d[i-1][2]+d[i-1][0]
+    d[i][0]%=mod
+    d[i][1]=d[i-1][0]+d[i-1][2]
+    d[i][1]%=mod
+    d[i][2]=d[i-1][0]+d[i-1][1]
+    d[i][2]%=mod
 
 print(sum(d[n])%mod)
