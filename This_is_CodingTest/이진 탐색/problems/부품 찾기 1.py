9585a8bfb4ebebc351a4d5f054abcb4e873822ee
carnival77 <stam0325@gmail.com>
2022-09-15 20:00:00 +0900

    review. 이코테. 이분 탐색. 2.부품 찾기

--- This_is_CodingTest/이진 탐색/problems/부품 찾기 1.py
+++ This_is_CodingTest/이진 탐색/problems/부품 찾기 1.py
@@ -1,23 +1,49 @@
-def binary_search(arr,tg,start,end):
-    if start>end:
-        return None
-    mid = (start+end)//2
-    if arr[mid] == tg:
-        return mid
-    elif arr[mid]>tg:
-        return binary_search(arr,tg,start,mid-1)
-    else:
-        return binary_search(arr,tg,mid+1,end)
+# def binary_search(arr,tg,start,end):
+#     if start>end:
+#         return None
+#     mid = (start+end)//2
+#     if arr[mid] == tg:
+#         return mid
+#     elif arr[mid]>tg:
+#         return binary_search(arr,tg,start,mid-1)
+#     else:
+#         return binary_search(arr,tg,mid+1,end)
+#
+# n = int(input())
+# arr = list(map(int,input().split()))
+#
+# m = int(input())
+# x= list(map(int,input().split()))
+#
+# for i in x:
+#     result = binary_search(arr,i,0,n-1)
+#     if result !=None:
+#         print("yes")
+#     else:
+#         print("no")
 
-n = int(input())
-arr = list(map(int,input().split()))
+def binary_search(arr,target,start,end):
+    while start<=end:
+        mid=(start+end)//2
+        if target==arr[mid]:
+            return mid
+        elif target<arr[mid]:
+            end=mid-1
+        else:
+            start=mid+1
+    return None
 
-m = int(input())
-x= list(map(int,input().split()))
+import sys
+input=sys.stdin.readline
+n=int(input())
+arr=list(map(int,input().split()))
+arr.sort()
+m=int(input())
+x=list(map(int,input().split()))
 
 for i in x:
-    result = binary_search(arr,i,0,n-1)
-    if result !=None:
-        print("yes")
+    res=binary_search(arr,i,0,n-1)
+    if res!=None:
+        print('yes',end=' ')
     else:
-        print("no")
+        print('no',end=' ')
