a5b8f55b36c1ae8525450acfe2809b59bf4307e8
carnival77 <stam0325@gmail.com>
2022-09-28 20:00:00 +0200

    review. 백준 강의 연습편 - 구현/시뮬레이션 - 16234.인구 이동.py

--- Baekjoon/강의/강의 연습편/시뮬레이션과 구현/second/16234.인구 이동.py
+++ Baekjoon/강의/강의 연습편/시뮬레이션과 구현/second/16234.인구 이동.py
@@ -1,75 +1,41 @@
 from collections import deque
 
-n,l,r = map(int,input().split())
+dx=[-1,0,1,0]
+dy=[0,1,0,-1]
 
-board=[]
-
-for _ in range(n):
-    board.append(list(map(int,input().split())))
-
-dy = [0,0,-1,1]
-dx = [-1,1,0,0]
-
-day_cnt=0
-
-# bfs 로 인구 이동 가능한 국가들을 연합으로 그룹 짓고, 같은 union_num을 부여한 뒤, 연합 내 인구 이동 실행.
-def process(x,y,union_num):
-    # 연합된 국가들
-    united=[]
-    united.append((x,y))
-    # 연합 국가들의 인구 수 합
-    sum = board[x][y]
-    # bfs 위한 연합 국가들 q
-    q = deque()
-    q.append((x,y))
-    # 현재 연합 번호 할당
-    union[x][y] = union_num
-
-    while q:
-        x,y = q.popleft()
-
-        # 상하좌우
-        for direction in range(4):
-            nx = x + dx[direction]
-            ny = y + dy[direction]
-
-            # 상하좌우 방향의 해당 칸이 맵 내에 있고 아직 union에 가입되지 않았다면
-            if nx >=0 and ny >= 0 and nx < n and ny < n and union[nx][ny] == -1:
-                # 인구 차이가 l 이상 r 이하라면
-                if l <= abs(board[x][y] - board[nx][ny])  <= r:
-                    # 같은 연합이므로
-                    # q에 삽입
-                    q.append((nx,ny))
-                    # united에 삽입
-                    united.append((nx,ny))
-                    # 연합 번호 할당
-                    union[nx][ny] = union_num
-
-                    sum += board[nx][ny]
-
-    # 연합 내 국가 간 인구 이동
-    for x,y in united:
-        board[x][y] = sum // len(united)
-
-while True:
-    # 매일마다 연합 갱신. 아직 연합이 정해지지 않은 곳은 -1
-    union = [[-1] * n for _ in range(n)]
-    # 연합 개수(번호). 이것이 n*n 과 같게 되어 연합이 전혀 없고 각 나라가 개인팀이 되어야 인구 이동 종료
-    union_num=0
-    # board 전체 체크
-    for x in range(n):
-        for y in range(n):
-            # 만약 연합이 정해지지 않은 곳이면
-            if union[x][y] == -1:
-                # process 실행. bfs 로 인구 이동 가능한 국가들을 연합으로 그룹 짓고, 같은 union_num을 부여한 뒤, 연합 내 인구 이동 실행.
-                process(x,y,union_num)
-                # 연합 개수(번호) +1
-                union_num += 1
-
-    # 연합 개수(번호). 이것이 n*n 과 같게 되어 연합이 전혀 없고 각 나라가 개인팀이 되어야 인구 이동 종료
-    if union_num == n*n:
+n,l,r=map(int,input().split())
+a=[list(map(int,input().split())) for _ in range(n)]
+ans=0
+for time in range(2001):
+    ok=False # 인구이동 발생 여부 체크
+    visit = [[False] * n for _ in range(n)] # 매일마다 국가별 방문 여부 체크
+    # 격자 탐색
+    for i in range(n):
+        for j in range(n):
+            # 만약 방문 안 했다면
+            if not visit[i][j]:
+                # 방문 표시
+                visit[i][j]=True
+                # BFS로 해당 칸에서 국경선 열 수 있는 국가 간에 연합 결성
+                union=[(i,j)]
+                cnt=1
+                s=a[i][j]
+                q=deque()
+                q.append((i,j))
+                while q:
+                    x,y=q.popleft()
+                    for k in range(4):
+                        nx,ny=x+dx[k],y+dy[k]
+                        if 0<=nx<n and 0<=ny<n and not visit[nx][ny] and l<=abs(a[nx][ny]-a[x][y])<=r:
+                            ok=True
+                            q.append((nx,ny))
+                            visit[nx][ny]=True
+                            s+=a[nx][ny]
+                            cnt+=1
+                            union.append((nx,ny))
+                for x,y in union:
+                    a[x][y]=s//cnt
+    if not ok:
+        ans=time
         break
-    # 한 번의 인구 이동이 종료되면 1일 +
-    day_cnt += 1
-
-print(day_cnt)
+print(ans)
