337dcc3d1461658e44fad9a9e3b7ee91a3ef7f66
carnival77 <stam0325@gmail.com>
2022-10-28 15:03:07 +0900

    review. 백준 강의 기초편 - DP - 11057.오르막 수.py

--- Baekjoon/강의/강의 기초편/Dynamic Programming/2/second/11057.오르막 수.py
+++ Baekjoon/강의/강의 기초편/Dynamic Programming/2/second/11057.오르막 수.py
@@ -1,22 +1,14 @@
-# 해설 : https://carnival.tistory.com/71
-
 n=int(input())
-
-d=[[0]*10 for _ in range(n+1)]
-
+d=[[0]*10 for _ in range(n+1)] # d[i][j]=i 자리가 j로 끝나는 오름차순 수의 개수
+# d[i][j]=d[i][j-1]+d[i-1][j] (1<=i<=n, 1<=j<=9)
 mod=10007
+for i in range(10):
+    d[1][i]=1
+for i in range(1,n+1):
+    d[i][0]=1
 
 for i in range(1,n+1):
-    for j in range(10):
-        if i==1:
-            d[i][j]=1
-        else:
-            for k in range(j+1):
-                d[i][j] += d[i-1][k]
+    for j in range(1,10):
+        d[i][j]=d[i][j-1]+d[i-1][j]
         d[i][j]%=mod
-
-ans=0
-for j in range(10):
-    ans+=d[n][j]
-
-print(ans%mod)
+print(sum(d[n])%mod)
