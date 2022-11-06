9da7650887e40b57312b4904deda51c921eb71e2
carnival77 <stam0325@gmail.com>
2022-11-06 23:00:00 +0900

    353. 백준 강의 연습편 - DP - 9252.LCS 2.py

---
+++ Baekjoon/강의/강의 연습편/다이나믹 프로그래밍/first/9252.LCS 2.py
@@ -0,0 +1,26 @@
+# d[i][j] : 수열 A의 i번째 문자까지, 수열 B의 j번째 문자까지 있을 때 LCS의 길이
+# d2[i][j] : 수열 A의 i번째 문자까지, 수열 B의 j번째 문자까지 있을 때 LCS
+import sys
+input=sys.stdin.readline
+a=' '+input().strip()
+b=' '+input().strip()
+n,m=len(a)-1,len(b)-1
+d=[[0]*(m+1) for _ in range(n+1)]
+d2=[['']*(m+1) for _ in range(n+1)]
+
+for i in range(1,n+1):
+    for j in range(1,m+1):
+        if a[i]==b[j]:
+            d[i][j]=d[i-1][j-1]+1
+            d2[i][j]=d2[i-1][j-1]+a[i]
+        else:
+            # d[i][j] = max(d[i][j - 1], d[i - 1][j])
+            if d[i][j-1]>d[i-1][j]:
+                d[i][j]=d[i][j-1]
+                d2[i][j]=d2[i][j-1]
+            else:
+                d[i][j]=d[i-1][j]
+                d2[i][j]=d2[i-1][j]
+
+print(d[n][m])
+print(d2[n][m])
