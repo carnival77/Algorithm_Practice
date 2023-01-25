d0b72cb4e3a6f86e1c99fbf52d8f013f732b453d
carnival77 <stam0325@gmail.com>
2023-01-25 22:00:00 +0900

    review. 백준 유형별 풀이 - 슬라이딩 윈도우 - 1593.문자 해독.py

--- Baekjoon/일반/슬라이딩 윈도우/1593.문자 해독.py
+++ Baekjoon/일반/슬라이딩 윈도우/1593.문자 해독.py
@@ -5,60 +5,102 @@
 # 2. s를 담을 때, 슬라이딩 윈도우 기법을 적용한다.
 # 3. 즉, s에 n개씩 담으면서, 담긴 문자열이 w와 같은 지 체크한다. 그리고 슬라이딩 윈도우 왼쪽 것은 빼고, 오른쪽 것은 새로 넣으면서 m까지 탐색한다.
 
-import sys
+# import sys
+#
+# input=sys.stdin.readline
+#
+# n,m = map(int,input().split())
+#
+# # w=input()
+# #
+# # s=input()
+#
+# w = sys.stdin.readline().strip()
+# s = sys.stdin.readline().strip()
+#
+# # print(w)
+#
+# ans=0
+#
+# # 알파벳 문자열. 0~25 까지의 26자리는 소문자 알파벳(a-z), 26~51까지의 26자리는 대문자 알파벳(A-Z)
+# wl = [0] * 52
+# sl = [0] * 52
+#
+# # 1. w 담기
+# for i in range(n):
+#     # a-z
+#     if 'a' <= w[i] <= 'z':
+#         wl[ord(w[i])-ord('a')]+=1
+#     # A-Z. 대문자 알파벳. 26을 더해 26번째부터 채워준다.
+#     else:
+#         wl[ord(w[i])-ord('A') + 26] +=1
+#
+# length,left=0,0
+# # 2. s 담기. 슬라이딩 윈도우 기법 적용
+# for i in range(m):
+#     # a-z
+#     if 'a' <= s[i] <= 'z':
+#         sl[ord(s[i])-ord('a')]+=1
+#     # A-Z. 대문자 알파벳. 26을 더해 26번째부터 채워준다.
+#     else:
+#         sl[ord(s[i])-ord('A') + 26] +=1
+#     length+=1
+#
+#     # 3. 즉, s에 n개씩 담으면서, 담긴 문자열이 w와 같은 지 체크한다. 그리고 슬라이딩 윈도우 왼쪽 것은 빼고, 오른쪽 것은 새로 넣으면서 m까지 탐색한다.
+#     if length==n:
+#         # 담긴 문자열 sl이 wl와 같은 지 체크한다.
+#         if sl==wl:
+#             ans+=1
+#         # 그리고 슬라이딩 윈도우 왼쪽 것은 빼고, 오른쪽 것은 새로 넣으면서 m까지 탐색한다.
+#         # a-z
+#         if 'a' <= s[left] <= 'z':
+#             sl[ord(s[left]) - ord('a')] -=1
+#         # A-Z. 대문자 알파벳. 26을 더해 26번째부터 채워준다.
+#         else:
+#             sl[ord(s[left]) - ord('A') + 26] -=1
+#         length-=1
+#         left+=1
+#
+# print(ans)
 
+import sys
 input=sys.stdin.readline
 
-n,m = map(int,input().split())
+n,m=map(int,input().split())
+w=list(input())
+s=list(input())
 
-# w=input()
-#
-# s=input()
-
-w = sys.stdin.readline().strip()
-s = sys.stdin.readline().strip()
-
-# print(w)
+a=[0]*52
+b=[0]*52
 
 ans=0
 
-# 알파벳 문자열. 0~25 까지의 26자리는 소문자 알파벳(a-z), 26~51까지의 26자리는 대문자 알파벳(A-Z)
-wl = [0] * 52
-sl = [0] * 52
-
-# 1. w 담기
 for i in range(n):
-    # a-z
-    if 'a' <= w[i] <= 'z':
-        wl[ord(w[i])-ord('a')]+=1
-    # A-Z. 대문자 알파벳. 26을 더해 26번째부터 채워준다.
+    if w[i].islower():
+        a[ord(w[i])-ord('a')]+=1
     else:
-        wl[ord(w[i])-ord('A') + 26] +=1
+        a[ord(w[i])-ord('A')+26]+=1
 
-length,left=0,0
-# 2. s 담기. 슬라이딩 윈도우 기법 적용
-for i in range(m):
-    # a-z
-    if 'a' <= s[i] <= 'z':
-        sl[ord(s[i])-ord('a')]+=1
-    # A-Z. 대문자 알파벳. 26을 더해 26번째부터 채워준다.
+for i in range(n):
+    if s[i].islower():
+        b[ord(s[i])-ord('a')]+=1
     else:
-        sl[ord(s[i])-ord('A') + 26] +=1
-    length+=1
+        b[ord(s[i])-ord('A')+26]+=1
 
-    # 3. 즉, s에 n개씩 담으면서, 담긴 문자열이 w와 같은 지 체크한다. 그리고 슬라이딩 윈도우 왼쪽 것은 빼고, 오른쪽 것은 새로 넣으면서 m까지 탐색한다.
-    if length==n:
-        # 담긴 문자열 sl이 wl와 같은 지 체크한다.
-        if sl==wl:
-            ans+=1
-        # 그리고 슬라이딩 윈도우 왼쪽 것은 빼고, 오른쪽 것은 새로 넣으면서 m까지 탐색한다.
-        # a-z
-        if 'a' <= s[left] <= 'z':
-            sl[ord(s[left]) - ord('a')] -=1
-        # A-Z. 대문자 알파벳. 26을 더해 26번째부터 채워준다.
-        else:
-            sl[ord(s[left]) - ord('A') + 26] -=1
-        length-=1
-        left+=1
+if a==b:
+    ans+=1
+
+for i in range(n,m):
+    j=i-n
+    if s[i].islower():
+        b[ord(s[i])-ord('a')]+=1
+    else:
+        b[ord(s[i])-ord('A')+26]+=1
+    if s[j].islower():
+        b[ord(s[j])-ord('a')]-=1
+    else:
+        b[ord(s[j])-ord('A')+26]-=1
+    if a==b:
+        ans+=1
 
 print(ans)
