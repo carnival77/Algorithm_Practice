56102e3fd4b5e05d67ad03257d43972f6b04d308
carnival77 <stam0325@gmail.com>
2023-01-29 22:00:00 +0900

    386. 백준 유형별 풀이 - 트리 - 1068.트리.py

--- Baekjoon/일반/트리/11725.트리의 부모 찾기.py
+++ Baekjoon/일반/트리/11725.트리의 부모 찾기.py
@@ -3,18 +3,18 @@ input=sys.stdin.readline
 sys.setrecursionlimit(10**6)
 
 n=int(input())
-graph=[[] for _ in range(n+1)]
+tree=[[] for _ in range(n+1)]
 visited=[False]*(n+1)
 ans=[0]*(n+1)
 
 for _ in range(1,n):
     a,b=map(int,input().split())
-    graph[a].append(b)
-    graph[b].append(a)
+    tree[a].append(b)
+    tree[b].append(a)
 
 def dfs(parent):
     visited[parent]=True
-    for child in graph[parent]:
+    for child in tree[parent]:
         if not visited[child]:
             ans[child]=parent
             dfs(child)
