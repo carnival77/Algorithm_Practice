da0a2e502219b99771c01da60b7670fd9a1fd981
carnival77 <stam0325@gmail.com>
2023-02-02 18:43:56 +0900

    review. 백준 강의 기초편 - 브루트 포스 - 재귀. 9095. 1,2,3 더하기.py

--- Baekjoon/강의/강의 기초편/Brute Force/재귀/first/9095. 1,2,3 더하기.py
+++ Baekjoon/강의/강의 기초편/Brute Force/재귀/first/9095. 1,2,3 더하기.py
@@ -1,32 +1,27 @@
-# t = int(input())
-#
-# def recur(cnt,sum,n):
-#     if sum>n : return 0
-#     if sum == n: return 1
-#     now=0
-#     for i in range(1,4):
-#         now += recur(cnt+1,sum+i,n)
-#     return now
-#
-# for i in range(t):
-#     n=int(input())
-#     print(recur(0,0,n))
-
-
-
+from itertools import product
 
 t = int(input())
 
-def dfs(sum,cnt,n):
-    if sum==n:
-        return 1
-    if sum>n:
-        return 0
-    answer = 0
-    for i in range(1,4):
-        answer += dfs(sum+i,cnt+1,n)
+# def dfs(sum,cnt,n):
+#     if sum==n:
+#         return 1
+#     if sum>n:
+#         return 0
+#     answer = 0
+#     for i in range(1,4):
+#         answer += dfs(sum+i,cnt+1,n)
+#
+#     return answer
+#
+# for i in range(t):
+#     print(dfs(0,0,int(input())))
 
-    return answer
-
-for i in range(t):
-    print(dfs(0,0,int(input())))
+arr=[1,2,3]
+for _ in range(t):
+    n=int(input())
+    ans=0
+    for i in range(1,n+1):
+        for pd in product(arr,repeat=i):
+            if sum(pd)==n:
+                ans+=1
+    print(ans)
