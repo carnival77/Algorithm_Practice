2635c0dcf54a098bada0955783c0dbf90f41354e
carnival77 <stam0325@gmail.com>
2023-02-16 19:53:08 +0900

    review. 백준 강의 기초편 - 다이나믹 프로그래밍 - 11057.오르막 수.py

---
+++ Baekjoon/강의/강의 기초편/Dynamic Programming/2/3rd/11057.오르막 수.py
@@ -0,0 +1,13 @@
+n=int(input())
+d=[[0]*10 for _ in range(n+1)] # d[i][j]=i 자리가 j로 끝나는 오름차순 수의 개수
+# d[i][j]=d[i][j-1]+d[i-1][j] (1<=i<=n, 1<=j<=9)
+mod=10007
+for i in range(10):
+    d[1][i]=1
+
+for i in range(1,n+1):
+    for j in range(10):
+        if j==0: d[i][j]=1
+        else: d[i][j]=d[i][j-1]+d[i-1][j]
+        d[i][j]%=mod
+print(sum(d[n])%mod)
--- Baekjoon/강의/강의 기초편/Dynamic Programming/2/3rd/1149.RGB 거리.py
+++ Baekjoon/강의/강의 기초편/Dynamic Programming/2/3rd/1149.RGB 거리.py
@@ -1,7 +1,7 @@
 n=int(input())
 
 a=[list(map(int,input().split())) for _ in range(n)]
-d=[[0]*3 for _ in range(n+1)]
+d=[[0]*3 for _ in range(n+1)] # i번째 집을 j로 칠했을 때 전체 최소 비용(j : 0(빨), 1(파), 2(초))
 
 d[1][0]=a[0][0]
 d[1][1]=a[0][1]
