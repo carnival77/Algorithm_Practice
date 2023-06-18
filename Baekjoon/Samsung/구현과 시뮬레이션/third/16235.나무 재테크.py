010ab6819a3f1471a65a1b26cfaf3fd190d3b535
carnival77 <stam0325@gmail.com>
2023-06-18 22:00:00 +0900

    review. 백준. 삼성 기출 - 16235.나무 재테크.py

--- Baekjoon/Samsung/구현과 시뮬레이션/third/16235.나무 재테크.py
+++ Baekjoon/Samsung/구현과 시뮬레이션/third/16235.나무 재테크.py
@@ -5,7 +5,6 @@ n,m,l=map(int,input().split())
 a=[list(map(int,input().split())) for _ in range(n)]
 b=[[5]*n for _ in range(n)]
 t=[[[] for _ in range(n)] for _ in range(n)]
-d=[]
 ans=0
 
 dx=[-1,-1,-1,0,0,1,1,1]
@@ -21,65 +20,32 @@ def inBoard(nx,ny):
     if 0<=nx<n and 0<=ny<n:
         return True
     return False
-    
-def spring():
-    global b,t,d
-    
-    d=[[[] for _ in range(n)] for _ in range(n)]
-    tmp=[[[] for _ in range(n)] for _ in range(n)]
-    
-    for x in range(n):
-        for y in range(n):
-            if len(t[x][y])>0:
-                t[x][y].sort()
-                for i in range(len(t[x][y])):
-                    if b[x][y]>=t[x][y][i]:
-                        b[x][y]-=t[x][y][i]
-                        t[x][y][i]+=1
-                        tmp[x][y].append(t[x][y][i])
-                    else:
-                        d[x][y].append(t[x][y][i])
-
-    t=tmp
-
-def summer():
-    global b
-    
-    for x in range(n):
-        for y in range(n):
-            if len(d[x][y])>0:
-                for dead in d[x][y]:
-                    b[x][y]+=dead//2
-
-def fall():
-    global t
-
-    for x in range(n):
-        for y in range(n):
-            if len(t[x][y]) > 0:
-                for tree in t[x][y]:
-                    if tree%5==0:
-                        for k in range(8):
-                            nx,ny=x+dx[k],y+dy[k]
-                            if not inBoard(nx,ny):continue
-                            t[nx][ny].append(1)
-
-def winter():
-    global b
-
-    for x in range(n):
-        for y in range(n):
-            b[x][y]+=a[x][y]
 
 for _ in range(l):
-    #봄
-    spring()
-    #여름
-    summer()
-    #가을
-    fall()
-    #겨울
-    winter()
+    p=[[0]*n for _ in range(n)]
+    for x in range(n):
+        for y in range(n):
+            lived = []
+            dead=0
+            t[x][y].sort()
+            for tree in t[x][y]:
+                if b[x][y]>=tree:
+                    b[x][y]-=tree
+                    lived.append(tree+1)
+                    if (tree+1) % 5 == 0:
+                        for k in range(8):
+                            nx, ny = x + dx[k], y + dy[k]
+                            if not inBoard(nx, ny): continue
+                            p[nx][ny]+=1
+                else:
+                    dead+=tree//2
+            t[x][y]=lived
+            b[x][y]+=dead
+            b[x][y] += a[x][y]
+    for x in range(n):
+        for y in range(n):
+            for _ in range(p[x][y]):
+                t[x][y].append(1)
 
 for i in range(n):
     for j in range(n):
