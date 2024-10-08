f6c14aeb37175800dbc1b616da52c45d4167c9b4
carnival77 <stam0325@gmail.com>
2024-10-08 22:00:00 +0900

    review. 백준 유형별 풀이 - 문자열 - 5525.IOIOI.py

---
+++ Baekjoon/일반/문자열/2nd/5525.IOIOI.py
@@ -0,0 +1,20 @@
+n=int(input())
+m=int(input())
+s=input()
+
+cnt=0 # IOI의 개수
+ans=0
+i=0
+
+while i<m:
+    if s[i:i+3]=='IOI':
+        i+=2
+        cnt+=1
+        if cnt==n:
+            ans+=1
+            cnt-=1
+    else:
+        i+=1
+        cnt=0
+
+print(ans)
