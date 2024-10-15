8f47ef72481ebff197030e7198dddbaa4b50e464
carnival77 <stam0325@gmail.com>
2024-10-15 22:00:00 +0900

    review. 백준 강의 기초편 - 다이나믹 프로그래밍 - 15990.1,2,3 더하기 5.py

--- Baekjoon/강의/강의 기초편/Dynamic Programming/1/4th/15990.1,2,3 더하기 5.py
+++ Baekjoon/강의/강의 기초편/Dynamic Programming/1/4th/15990.1,2,3 더하기 5.py
@@ -1,14 +1,12 @@
-MAX=100001
-mod=1000000009
 t=int(input())
-d=[[0]*4 for _ in range(MAX)]
+d=[[0]*4 for _ in range(100001)]
+mod=1000000009
+
 d[1][1]=1
 d[2][2]=1
-d[3][3]=1
-d[3][2]=1
-d[3][1]=1
+d[3][1]=d[3][2]=d[3][3]=1
 
-for i in range(4,MAX):
+for i in range(4,100001):
     for j in range(1,4):
         if j==1:
             d[i][j]=d[i-1][2]+d[i-1][3]
@@ -18,6 +16,6 @@ for i in range(4,MAX):
             d[i][j]=d[i-3][1]+d[i-3][2]
         d[i][j]%=mod
 
-for i in range(t):
+for _ in range(t):
     n=int(input())
     print(sum(d[n])%mod)
